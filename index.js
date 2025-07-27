ad823d600bb162c44e7dfecf116d4235e711768f
ayman <danishaiman3b@gmail.com>
2025-07-27 22:57:37 +0800

    chore: add express as a dependency in package.json

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const RonaldoBot = require('./src/bot');
+const express = require('express');
+
+// Create Express app for keeping the service alive
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Basic route to confirm the bot is alive
+app.get('/', (req, res) => {
+    res.send('Ronaldo Bot is alive');
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+    console.log(`Express server is running on port ${PORT}`);
+});
 
 // Create and start the bot
 const bot = new RonaldoBot();
